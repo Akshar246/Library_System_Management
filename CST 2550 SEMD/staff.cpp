76f3382164e56a28ff50d7a04e9d935f3d693822
Akshar246 <148439748+Akshar246@users.noreply.github.com>
2024-01-16 02:01:48 +0530

    first commit staff.cpp

--- CST 2550 SEMD/staff.cpp
+++ CST 2550 SEMD/staff.cpp
@@ -1,12 +1,17 @@
+// staff.cpp
+
 #include "staff.h"
 
+// Constructor to initialize staff member with provided information
 staff::staff(const std::string& name, const std::string& addr, const std::string& email, const std::string& pwd)
     : fullName(name), address(addr), emailID(email), password(pwd) {}
 
+// Login function to verify staff member's credentials
 bool staff::login(const std::string& email, const std::string& pwd) const {
     return (email == emailID && pwd == password);
 }
 
+// Getter function to retrieve staff member's full name
 std::string staff::getFullName() const {
     return fullName;
 }
@@ -24,6 +29,7 @@ std::string staff::getPassword() const
     return password;
 }
 
+// Getter function to retrieve staff member's staff ID
 std::string staff::getStaffId() const
 {
     return staffId;
