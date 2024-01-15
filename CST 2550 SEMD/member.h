d61ecda796450effa063bc1b1cb8be6c59e0f926
Akshar246 <148439748+Akshar246@users.noreply.github.com>
2024-01-16 02:05:21 +0530

    Second commit member.h

--- CST 2550 SEMD/member.h
+++ CST 2550 SEMD/member.h
@@ -3,34 +3,54 @@
 #ifndef MEMBER_H
 #define MEMBER_H
 
+#include <iostream>
 #include <string>
 #include <vector>
+#include "book.h"
 
-class Member {
+class Member
+{
 private:
     std::string name;
     std::string address;
     std::string email;
     std::string password;
+    bool isLoggedIn;
 
 public:
-    // Constructors
     Member();
-    Member(const std::string& name, const std::string& address, const std::string& email, const std::string& password);
+
+    // Parameterized constructor
+    Member(const std::string &name, const std::string &address, const std::string &email, const std::string &password);
 
     // Getter methods
     std::string getFullName() const;
     std::string getAddress() const;
     std::string getEmailID() const;
     std::string getPassword() const;
+    bool getIsLoggedIn() const;
 
-    // Member functions
-    bool login(const std::string& email, const std::string& pass) const;
+    // Member login
+    bool login(const std::string &email, const std::string &pass);
+
+    // Display borrowed books for the current member
+    void displayBorrowedBooks() const;
+
+    // Save member information to a text file
     void saveMemberToTxt() const;
-    void inputMemberData();  // Added this function
 
-    // Static method for login and register
+    // Input member data during registration
+    void inputMemberData();
+
+    // Static function to handle member login or registration
     static Member loginOrRegister();
+
+    // Static function to check if a member is logged in
+    static bool isMemberLoggedIn(const Member &member);
+
+    static std::vector<Member> loadMembersFromTxt();
+
+    
 };
 
 #endif // MEMBER_H
